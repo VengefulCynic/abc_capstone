d1d6242f04280e0097b00ee7adb6e17849f575a8
elbarrio13 <60749899+elbarrio13@users.noreply.github.com>
2020-02-06 16:12:48 -0600

    Update MainActivity.java

--- accelerationtester/src/com/capstone/accelerationtester/MainActivity.java
+++ accelerationtester/src/com/capstone/accelerationtester/MainActivity.java
@@ -16,13 +16,14 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName() + " ";
     private Timer timer;
     private TimerTask task;
+    private TextView answerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        answerLabel = (TextView) findViewById(R.id.textView1);
 
         Button readacceleration = (Button) findViewById(R.id.readacceleration); 
         readacceleration.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +31,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Log.i(TAG, "onCreate readacceleration onClick+");
-                answerLabel.setText(readFiles());
+                readFiles();
                 Log.i(TAG, "onCreate readacceleration onClick-");
                     
             }
@@ -58,7 +59,7 @@ public class MainActivity extends Activity {
                     public void run() { 
                         // do your work
                         Log.i(TAG, "onCreate startmonitor onClick task to schedule a readAccelerometer() each 20 seconds");
-                        answerLabel.setText(readFiles());
+                        readFiles();
                     } 
                 }; 
                 timer.schedule(task, 0, 60*(1000*20));// execute it each 20 seconds
@@ -88,13 +89,13 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    public String readFiles() {
+    public void readFiles() {
         String x = readFile("/sys/bus/i2c/devices/1-0018/x_axis");
         String y = readFile("/sys/bus/i2c/devices/1-0018/y_axis");
         String z = readFile("/sys/bus/i2c/devices/1-0018/z_axis");
         String ret = "Read Acceleration x=" + (Double.valueOf(x)/1000) + ", y=" + (Double.valueOf(y)/1000) + ", z=" + (Double.valueOf(y)/1000);
+        this.answerLabel.setText(ret);
         Log.i(TAG, ret);
-        return ret;
     }
 
     public String readFile(String path) {
